cf20fd9643e46702517108eb01710478c2a761be
Giov3ss <giovani.sp25@gmail.com>
2022-11-09 18:24:01 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,8 +52,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Hey you got it right! :D')
+        incrementScore();
     } else {
         alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -80,11 +82,25 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the currrent score from the DOM and increments it by 1
+ */
+
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the currrent tally of incorrect answers from the DOM and increments it by 1
+ */
+
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 
 }
 
